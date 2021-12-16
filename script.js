10a9d727881d86740b22fd3bcdabe03f16b7d45b
Jose Thiago Lisboa de Almeida <jose.almeida@engeseltserv.com>
2021-12-16 16:37:52 -0300

    Ajuste no layout

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function imagemTexto(mg) {
-    document.getElementById('imagem').src= mg;
+function imagemTexto(mg,nu) {
     
-
+    document.getElementById('imagem').src= mg;
+    document.getElementById('paragrafo').innerHTML=lista_p[nu];
 }
+
+var lista_p = Array()
+lista_p[1] = 'Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo (Exemplo: alguns livros digitais são disponibilizados no GitHub e escrito aos poucos publicamente). O Git foi inicialmente projetado e desenvolvido por Linus Torvalds para o desenvolvimento do kernel Linux, mas foi adotado por muitos outros projetos. Cada diretório de trabalho do Git é um repositório com um histórico completo e habilidade total de acompanhamento das revisões, não dependente de acesso a uma rede ou a um servidor central.'
+lista_p[2] = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+lista_p[3] = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+lista_p[4] = 'xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+lista_p[5] = 'xa'
+lista_p[6] = 'xa'
+lista_p[7] = 'xa'
+lista_p[8] = 'xa'
+lista_p[9] = 'xa'
+lista_p[10] = 'xa'
